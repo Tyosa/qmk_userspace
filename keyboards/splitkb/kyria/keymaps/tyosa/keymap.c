33bfd6c6c8896631ab32f1f5b8390d3bfcf2743e
Mael VILLERBU <mael.villerbu.ext@orange.com>
2024-05-29 17:03:32 +0200

    switch back to hrm for modifiers, add tab esc del bspc and enter to outer column

--- keyboards/splitkb/kyria/keymaps/tyosa/keymap.c
+++ keyboards/splitkb/kyria/keymaps/tyosa/keymap.c
@@ -5,27 +5,27 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT(
-            XXXXXXX,   Q_ESC,    KC_W,    KC_F,    KC_P,    KC_B,                                       J_ENT,    KC_L,    KC_U,    KC_Y, KC_SCLN, XXXXXXX,
-            XXXXXXX,   HR_GA,   HR_AR,    KC_S,    KC_T,    KC_G,                                       M_BSP,    KC_N,    KC_E,   HR_AI,   HR_GO, XXXXXXX,
-            XXXXXXX,   Z_TAB,    KC_X,    KC_C,    KC_D,   V_DEL, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX,    KC_K,    KC_H, COM_MIN, DOT_UND, SLS_EXC, XXXXXXX,
+             KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                        KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_ENT,
+             KC_TAB,   HR_GA,   HR_AR,   HR_CS,   HR_ST,    KC_G,                                        KC_M,   HR_SN,   HR_CE,   HR_AI,   HR_GO, KC_BSPC,
+            XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX,    KC_K,    KC_H, COM_MIN, DOT_UND, SLS_EXC,  KC_DEL,
                                        XXXXXXX, XXXXXXX, XXXXXXX, MEH_SPC, OS_LSFT, OS_LCTL,     SYM, XXXXXXX, XXXXXXX, XXXXXXX
             ),
     [_SYM] = LAYOUT(
-            XXXXXXX, KC_PERC,   KC_AT, KC_HASH,  KC_DLR,  KC_ESC,                                     KC_ENT, KC_AMPR, KC_ASTR,  KC_GRV, KC_CIRC, XXXXXXX,
-            XXXXXXX, QC_AGRV, QC_EGRV, QC_EAIG, XXXXXXX,  KC_TAB,                                    KC_BSPC, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, QC_CCED, XXXXXXX,  KC_DEL, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX, QC_QUOT, KC_BSLS, KC_LCBR, KC_RCBR,     NAV, XXXXXXX,
+             KC_ESC, KC_PERC,   KC_AT, KC_HASH,  KC_DLR, XXXXXXX,                                     XXXXXXX, KC_AMPR, KC_ASTR,  KC_GRV, KC_CIRC,  KC_ENT,
+             KC_TAB, QC_AGRV, QC_EGRV, QC_EAIG, XXXXXXX, XXXXXXX,                                     XXXXXXX, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_BSPC,
+            XXXXXXX, XXXXXXX, XXXXXXX, QC_CCED, XXXXXXX, XXXXXXX, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX, QC_QUOT, KC_BSLS, KC_LCBR, KC_RCBR,     NAV,  KC_DEL,
                                        XXXXXXX, XXXXXXX, XXXXXXX, COLEMAK, OS_LSFT, KC_RALT,     NUM, XXXXXXX, XXXXXXX, XXXXXXX
             ),
     [_NAV] = LAYOUT(
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_ESC,                                      KC_ENT,    PREV,    PTAB,    NTAB,    NEXT, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_TAB,                                     KC_BSPC, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX,  KC_END, XXXXXXX,
+             KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX,    PREV,    PTAB,    NTAB,    NEXT,  KC_ENT,
+             KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSPC,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, XXXXXXX,  KC_END,  KC_DEL,
                                        XXXXXXX, XXXXXXX, XXXXXXX, COLEMAK, KC_LSFT, KC_RCTL,     SYM, XXXXXXX, XXXXXXX, XXXXXXX
             ),
     [_NUM] = LAYOUT(
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_ESC,                                      KC_ENT,    KC_7,    KC_8,    KC_9, KC_MINS, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PDOT,  KC_TAB,                                     KC_BSPC,    KC_4,    KC_5,    KC_6, KC_EQL, XXXXXXX,
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PAST,  KC_DEL, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX,    KC_0,    KC_1,    KC_2,    KC_3,     NAV, XXXXXXX,
+             KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_PAST,    KC_7,    KC_8,    KC_9, KC_MINS,  KC_ENT,
+             KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_PDOT,    KC_4,    KC_5,    KC_6,  KC_EQL,  KC_BSPC,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LENC, KC_RENC, XXXXXXX,    KC_0,    KC_1,    KC_2,    KC_3,     NAV,  KC_DEL,
                                        XXXXXXX, XXXXXXX, XXXXXXX, COLEMAK, OS_LSFT, OS_LCTL,     SYM, XXXXXXX, XXXXXXX, XXXXXXX
             ),
 };
