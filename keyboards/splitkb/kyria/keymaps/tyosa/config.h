561613369862659cff977022645cc58bcd26913c
Maël Villerbu <mael.villerbu@proton.me>
2025-02-07 19:14:28 +0100

    Switch to Chordal Hold

--- keyboards/splitkb/kyria/keymaps/tyosa/config.h
+++ keyboards/splitkb/kyria/keymaps/tyosa/config.h
@@ -1,40 +1,14 @@
-/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #define SPLIT_WPM_ENABLE
 #define SPLIT_ACTIVITY_ENABLE
 
-#define TAPPING_TERM 150
-
 #define SPLIT_TRANSACTION_IDS_USER USER_SYNC_LUNA
 #define SPLIT_LED_STATE_ENABLE
 
-#define ONESHOT_TAP_TOGGLE 4
-#define ONESHOT_TIMEOUT 1500
-
-// Bilateral combinations, following https://sunaku.github.io/home-row-mods.html
-#define BILATERAL_COMBINATIONS
-#define BILATERAL_COMBINATIONS_ALLOW_SAMESIDED_AFTER 200 /*ms*/
-#define BILATERAL_COMBINATIONS_ALLOW_CROSSOVER_AFTER 50 /*ms*/
-#define BILATERAL_COMBINATIONS_DELAY_MODS_THAT_MATCH (MOD_MASK_GUI) /*Delay GUI HRM*/
-#define BILATERAL_COMBINATIONS_DELAY_MATCHED_MODS_BY 120  /* ms */
-#define BILATERAL_COMBINATIONS_TYPING_STREAK_TIMEOUT 160
-#define BILATERAL_COMBINATIONS_TYPING_STREAK_MODMASK (~MOD_MASK_SHIFT)
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
 
 // Space optimizations
 #define LAYER_STATE_8BIT
