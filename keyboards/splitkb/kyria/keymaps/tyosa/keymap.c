2bcd7ba3a589e0a752786603affb1c8b414a9266
Maël Villerbu <mael.villerbu@proton.me>
2025-02-07 19:14:28 +0100

    Switch to Chordal Hold

--- keyboards/splitkb/kyria/keymaps/tyosa/keymap.c
+++ keyboards/splitkb/kyria/keymaps/tyosa/keymap.c
@@ -82,3 +82,9 @@ void matrix_scan_user(void) {
   }
 }
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
+    'L', 'L', 'L', 'L', 'L', 'L',                     'R', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L', 'L',                     'R', 'R', 'R', 'R', 'R', 'R',
+    'L', 'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
+                   'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R'
+);
